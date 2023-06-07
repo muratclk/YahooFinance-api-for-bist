df59fb9d894fc9147a78fb732bb3de5c78a6e3e5
muratclk <cashkl@yandex.com>
2023-06-07 23:27:11 +0300

    added 10 most increased

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
+// const yahooFinance = require("yahoo-finance2").default;
+// const stocks = require("./stocks");
+
+// async function fetchStockData(symbol) {
+//   try {
+//     const quote = await yahooFinance.quote(symbol);
+//     // console.log(`${JSON.stringify(quote)}`);
+//     console.log(`Symbol: ${quote.symbol}, Price: ${quote.regularMarketPrice}`);
+//   } catch (error) {
+//     console.error(`Error fetching data for ${symbol}: `, error);
+//   }
+// }
+
+// async function fetchStockAllData(symbol) {
+//   try {
+//     const quote = await yahooFinance.quote(symbol);
+//     console.log(`${JSON.stringify(quote)}`);
+//     //regularMarketChangePercent
+
+//     // console.log(`Symbol: ${quote.symbol}, Price: ${quote.regularMarketPrice}`);
+//   } catch (error) {
+//     console.error(`Error fetching data for ${symbol}: `, error);
+//   }
+// }
+
+// // fetchStockAllData("EUPWR.IS");
+// stocks.forEach(fetchStockData);
+
 const yahooFinance = require("yahoo-finance2").default;
 const stocks = require("./stocks");
+
+let stockData = [];
 
 async function fetchStockData(symbol) {
   try {
     const quote = await yahooFinance.quote(symbol);
-    // console.log(`${JSON.stringify(quote)}`);
-    console.log(`Symbol: ${quote.symbol}, Price: ${quote.regularMarketPrice}`);
+    stockData.push({
+      symbol: quote.symbol,
+      changePercent: quote.regularMarketChangePercent,
+    });
   } catch (error) {
     console.error(`Error fetching data for ${symbol}: `, error);
   }
 }
 
-stocks.forEach(fetchStockData);
+Promise.all(stocks.map(fetchStockData)).then(() => {
+  // Sort by changePercent
+  stockData.sort((a, b) => b.changePercent - a.changePercent);
+
+  console.log("Top 10 Increased Stocks:");
+  for (let i = 0; i < 10; i++) {
+    console.log(
+      `Symbol: ${stockData[i].symbol}, Change Percent: ${stockData[i].changePercent}`
+    );
+  }
+
+  console.log("\nTop 10 Decreased Stocks:");
+  for (let i = stockData.length - 1; i >= stockData.length - 10; i--) {
+    console.log(
+      `Symbol: ${stockData[i].symbol}, Change Percent: ${stockData[i].changePercent}`
+    );
+  }
+});
